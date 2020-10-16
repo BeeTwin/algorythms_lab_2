a129525963d245c56bc01f49a1e24db96b3e1e8b
Danila Botov <dan.botov@gmail.com>
2020-10-16 23:41:12 +0500

    mc

--- algorythms_lab_2/TxtStackExecutor.cs
+++ algorythms_lab_2/TxtStackExecutor.cs
@@ -1,18 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace algorythms_lab_2
 {
-    public class TxtStackExecutor
+    public class TxtStackExecutor<T>
     {
-        private string _path;
-
-        public TxtStackExecutor(string path)
+        private class ParseItem
         {
-            _path = path;
+            public StackAction StackAction;
+            public T Value;
+        }
+
+        private enum StackAction
+        {
+            Push,
+            Pop,
+            Peek,
+            Any,
+            ToString
+        }
+
+        private Stack<T> _stack = new Stack<T>();
+        private List<ParseItem> _parseItems = new List<ParseItem>();
+        private Dictionary<StackAction, Action<T>> _dict;
+
+        public TxtStackExecutor()
+        {
+            Initialize();
+        }
+
+        public void Append(string path)
+        {
+            _parseItems = _parseItems.Concat(Parse(path)).ToList();
+        }
+
+        public void Execute()
+        {
+            foreach(var parseItem in _parseItems)
+                _dict[parseItem.StackAction](parseItem.Value);
+        }
+
+        private void Initialize()
+        {
+            _dict = new Dictionary<StackAction, Action<T>>();
+            _dict[StackAction.Push]     = (value) => _stack.Push(value);
+            _dict[StackAction.Pop]      = (_)     => _stack.Pop();
+            _dict[StackAction.Peek]     = (_)     => _stack.Peek();
+            _dict[StackAction.Any]      = (_)     => _stack.Any();
+            _dict[StackAction.ToString] = (_)     => Console.WriteLine(_stack);
+        }
+
+        private List<ParseItem> Parse(string path)
+        {
+            return null;
         }
     }
 }
